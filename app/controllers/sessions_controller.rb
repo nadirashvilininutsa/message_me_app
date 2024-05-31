608a186aaf2f39c5a8c2010119073dd68fbbebc5
Ninutsa <ninutsa.nadirashvili@gmail.com>
2024-06-01 01:36:50 +0400

    Add flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
     end
 
     def create
-      user = User.find_by(username: params[:session][:username]);
+      user = User.find_by(username: params[:session][:username].downcase);
       if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user_id
+        session[:user_id] = user.id
         flash[:success] = "You have successfully logged in"
         redirect_to root_path 
       else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :messages
+  before_save { self.username = username.downcase }
   validates :username, presence: true, length: { minimum: 3, maximum: 15 },
             uniqueness: { case_sensitive: false }
   has_secure_password
